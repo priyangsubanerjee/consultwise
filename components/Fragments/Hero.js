c0542c3e2a28d6b0813287225720574a85f19630
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-07-10 18:23:17 +0530

    content

--- components/Fragments/Hero.js
+++ components/Fragments/Hero.js
@@ -24,7 +24,7 @@ function HeroSection() {
 
           <div>
             <p className="font-light leading-8 text-sm">
-              Transform Your Digital Presence with Consultwise <br /> <br />
+              Transform Your Digital Presence with Consultwise <br />
               Are you ready to take your online presence to new heights? Look no
               further than Consultwise, your trusted partner in all things
               digital. We are a creative digital agency specializing in helping
@@ -32,6 +32,25 @@ function HeroSection() {
               innovative strategies and expert team, we are here to guide you
               towards achieving your online goals.
             </p>
+
+            <p className="text-base font-medium mt-7">
+              Ready to Ignite Your Digital Journey?
+            </p>
+
+            <p className="font-light leading-8 text-sm mt-5">
+              TWhether you are a startup looking to establish your online
+              presence or an established brand seeking to enhance your digital
+              footprint, Consultwise is here to assist you. Let us help you
+              unlock the full potential of your business in the digital
+              landscape.
+              <br />
+              <br /> Contact us today to schedule a consultation with our
+              experts and discover how Consultwise can transform your digital
+              presence into a powerful asset that drives growth, engagement, and
+              success. Together, let&apos;s embark on a journey of digital
+              excellence.
+            </p>
+
             <div className="mt-10">
               <p className="font-bebas text-base mb-5 text-neutral-500">
                 TRUSTED BY
